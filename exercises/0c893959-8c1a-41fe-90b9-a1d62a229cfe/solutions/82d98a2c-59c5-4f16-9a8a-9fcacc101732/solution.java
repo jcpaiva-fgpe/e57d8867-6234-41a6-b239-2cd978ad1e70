fccc0dd56880992135cb972a7b4ac6e51248654f
Ciro De Vita <cirogiuseppe.devita@uniparthenope.it>
2020-08-24 15:40:20 +0100

    created file exercises/0c893959-8c1a-41fe-90b9-a1d62a229cfe/solutions/82d98a2c-59c5-4f16-9a8a-9fcacc101732/solution.java

---
+++ exercises/0c893959-8c1a-41fe-90b9-a1d62a229cfe/solutions/82d98a2c-59c5-4f16-9a8a-9fcacc101732/solution.java
@@ -0,0 +1,16 @@
+public static Integer largestPrimeFactor(Integer n) {
+    int factor = -1;
+    for (int i = 2; i * i <= n; i++) {
+        if (n == 1) 
+            break;
+        if (n % i != 0) 
+            continue;
+        
+        factor = i;
+        while (n % i == 0) {
+            n /= i;
+        }
+    }
+
+    return n == 1 ? factor : n;
+}
