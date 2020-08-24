8173bd11048e2f6469a55e1d07e089c7a6ff3cc2
Ciro De Vita <cirogiuseppe.devita@uniparthenope.it>
2020-08-24 15:49:55 +0100

    created file exercises/bad4d4eb-499a-4cff-b743-377e5083d8b0/templates/f74b6355-f278-4ac3-9d51-cb32e603c2e7/template.java

---
+++ exercises/bad4d4eb-499a-4cff-b743-377e5083d8b0/templates/f74b6355-f278-4ac3-9d51-cb32e603c2e7/template.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class template {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int name = reader.nextInt();
+
+        System.out.println(moran(name));
+    }
+
+    {{code}}
+}
