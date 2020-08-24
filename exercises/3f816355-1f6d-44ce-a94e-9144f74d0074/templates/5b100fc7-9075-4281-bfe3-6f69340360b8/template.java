6341ccff1b2995730643acda14eb69ffd5d5fe9e
Ciro De Vita <cirogiuseppe.devita@uniparthenope.it>
2020-08-24 15:57:51 +0100

    created file exercises/3f816355-1f6d-44ce-a94e-9144f74d0074/templates/5b100fc7-9075-4281-bfe3-6f69340360b8/template.java

---
+++ exercises/3f816355-1f6d-44ce-a94e-9144f74d0074/templates/5b100fc7-9075-4281-bfe3-6f69340360b8/template.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class template {
+	public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String name = reader.nextLine();
+
+        System.out.println(nameShuffle(name));
+    }
+
+    
+    {{code}}
+}
