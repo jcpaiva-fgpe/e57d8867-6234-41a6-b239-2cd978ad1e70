806214aa409a966265f1bbc1e31e49544c304af1
Ciro De Vita <cirogiuseppe.devita@uniparthenope.it>
2020-08-24 16:34:53 +0100

    created file exercises/16173d12-85f5-468b-b282-0dc2fa5a9e5b/templates/e1377893-0b67-4c55-867d-57a148e9889c/template.c

---
+++ exercises/16173d12-85f5-468b-b282-0dc2fa5a9e5b/templates/e1377893-0b67-4c55-867d-57a148e9889c/template.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+{{code}}
+
+
+int main() {
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+ 
+    return 0;
+}
