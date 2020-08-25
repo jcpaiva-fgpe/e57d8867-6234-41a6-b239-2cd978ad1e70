5d1563928f3ae74cda8610a1fcbaed4605280826
Ciro De Vita <cirogiuseppe.devita@uniparthenope.it>
2020-08-25 10:05:17 +0100

    created file exercises/0707c7fe-7596-41bc-9b38-d124bc8ec964/solutions/364c9e3e-7ad4-45ac-aa37-fe9e6a01e38f/solution.cpp

---
+++ exercises/0707c7fe-7596-41bc-9b38-d124bc8ec964/solutions/364c9e3e-7ad4-45ac-aa37-fe9e6a01e38f/solution.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <vector>
+ 
+template<typename ForwardIterator> void counting_sort(ForwardIterator begin, ForwardIterator end) {
+  {{code}}
+  {{code}}
+  {{code}}
+}
+ 
+int main() {
+  int a[] = {125, 0, 695, 3, -256, -5, 214, 44, 55};
+
+  copy(std::begin(a), std::end(a), std::ostream_iterator<int>(std::cout, " "));
+  std::cout << "\n";
+}
