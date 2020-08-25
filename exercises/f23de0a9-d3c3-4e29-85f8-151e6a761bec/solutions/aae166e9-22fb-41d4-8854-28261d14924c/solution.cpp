05bcfbb495e153d77c23575ccbac353a81f9191d
Ciro De Vita <cirogiuseppe.devita@uniparthenope.it>
2020-08-25 10:00:07 +0100

    created file exercises/f23de0a9-d3c3-4e29-85f8-151e6a761bec/solutions/aae166e9-22fb-41d4-8854-28261d14924c/solution.cpp

---
+++ exercises/f23de0a9-d3c3-4e29-85f8-151e6a761bec/solutions/aae166e9-22fb-41d4-8854-28261d14924c/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n,d;
+    cin>>n>>d;
+    
+    int a[n];
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<a[(i+d)%n]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
+
