00e694925e67e96c049a06af8b458f37b13f2772
Ciro De Vita <cirogiuseppe.devita@uniparthenope.it>
2020-08-25 10:01:55 +0100

    created file exercises/0bf38a50-2647-444e-89c4-d98590717a62/templates/065bb11f-a2b8-4603-a76b-eba2ff674153/template.cpp

---
+++ exercises/0bf38a50-2647-444e-89c4-d98590717a62/templates/065bb11f-a2b8-4603-a76b-eba2ff674153/template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+{{code}}
+
+int main(){
+    int n, m;
+    bool d;
+
+    cin>>n>>m>>d;
+    
+    cout<<bitRotate(n, m, d)<<endl;
+}
