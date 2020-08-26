d80079c9857597141402014392c418634460c5cb
Gennaro Mellone <gennaro.mellone@uniparthenope.it>
2020-08-26 12:51:02 +0100

    created file exercises/ebf4847f-f157-4fc3-8278-5b25eaaa1187/solutions/0af87f0d-714a-4548-8d96-5cb68ab59e43/solution.cpp

---
+++ exercises/ebf4847f-f157-4fc3-8278-5b25eaaa1187/solutions/0af87f0d-714a-4548-8d96-5cb68ab59e43/solution.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    float a,b,x;
+    cin>>a>>b;
+    if (a!=0) {
+        x=-b/a;
+        cout<<x<<endl;
+    }
+    else {
+        cout<<"IMPOSSIBLE"<<endl;
+        
+    }
+   return (0); }
