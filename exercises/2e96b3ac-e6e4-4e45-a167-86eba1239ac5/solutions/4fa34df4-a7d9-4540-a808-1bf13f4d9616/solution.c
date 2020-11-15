f115ee40ad79860ce22cab73ebcc8080dc8ee751
Gennaro Mellone <gennaro.mellone@uniparthenope.it>
2020-11-15 10:12:56 +0000

    created file exercises/2e96b3ac-e6e4-4e45-a167-86eba1239ac5/solutions/4fa34df4-a7d9-4540-a808-1bf13f4d9616/solution.c

---
+++ exercises/2e96b3ac-e6e4-4e45-a167-86eba1239ac5/solutions/4fa34df4-a7d9-4540-a808-1bf13f4d9616/solution.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    int a;
+    scanf("%d", &a);
+    
+    switch (a) {
+        case 1:
+            printf("JANUARY\n");
+            break;
+        case 2:
+            printf("FEBRUARY\n");
+            break;
+        case 3:
+            printf("MARCH\n");
+            break;
+        case 4:
+            printf("APRIL\n");
+            break;
+        case 5:
+            printf("MAY\n");
+            break;
+        case 6:
+            printf("JUNE\n");
+            break;
+        case 7:
+            printf("JULY\n");
+            break;
+        case 8:
+            printf("AUGUST\n");
+            break;
+        case 9:
+            printf("SEPTEMBER\n");
+            break;
+        case 10:
+            printf("OCTOBER\n");
+            break;
+        case 11:
+            printf("NOVEMBER\n");
+            break;
+        case 12:
+            printf("DECEMBER\n");
+            break;
+            
+        default:
+            printf("ERROR\n");
+            break;
+    }
+    
+    exit(0);
+}
