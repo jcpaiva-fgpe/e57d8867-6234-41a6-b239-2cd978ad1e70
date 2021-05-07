380a1bbff889efe7d0e6cdb5d4579cbb9707cb38
Ciro De Vita <cirogiuseppe.devita@uniparthenope.it>
2021-05-07 13:08:14 +0100

    updated file exercises/6d1485b7-2cda-42b8-8474-d216c593ce23/skeletons/80b714e5-f1ea-4258-8a00-21ea1ecb5d01/skeleton.c

--- exercises/6d1485b7-2cda-42b8-8474-d216c593ce23/skeletons/80b714e5-f1ea-4258-8a00-21ea1ecb5d01/skeleton.c
+++ exercises/6d1485b7-2cda-42b8-8474-d216c593ce23/skeletons/80b714e5-f1ea-4258-8a00-21ea1ecb5d01/skeleton.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-
 // YOUR CODE HERE
-
-
-int main(void) {
-   char a[100];
-    
-    scanf("%s",a);
-
-    alltoupper(a);
-    printf("%s\n",a);
-    exit(0);
-}
