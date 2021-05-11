0bd4f54bec3dde2a82a1fd6b7408ff543288d92f
Ciro De Vita <cirogiuseppe.devita@uniparthenope.it>
2021-05-11 15:23:31 +0100

    updated file exercises/9c05686f-4c04-41bf-ba7c-1dd2eba34d5f/templates/07102dec-35c7-4b4c-a1dd-7bed36669b0d/template.java

--- exercises/9c05686f-4c04-41bf-ba7c-1dd2eba34d5f/templates/07102dec-35c7-4b4c-a1dd-7bed36669b0d/template.java
+++ exercises/9c05686f-4c04-41bf-ba7c-1dd2eba34d5f/templates/07102dec-35c7-4b4c-a1dd-7bed36669b0d/template.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class template {
+public class solution {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         String word = s.nextLine();
