d8d60f7cd9fce5aa959958f23965d79374374eaf
Ciro De Vita <cirogiuseppe.devita@uniparthenope.it>
2021-05-16 08:11:55 +0100

    updated file exercises/7d24a469-91fa-4a06-847d-259771d3de07/skeletons/f5eca602-c3be-4b2b-b9c7-5a145783653b/skeleton.java

--- exercises/7d24a469-91fa-4a06-847d-259771d3de07/skeletons/f5eca602-c3be-4b2b-b9c7-5a145783653b/skeleton.java
+++ exercises/7d24a469-91fa-4a06-847d-259771d3de07/skeletons/f5eca602-c3be-4b2b-b9c7-5a145783653b/skeleton.java
@@ -1,18 +1,18 @@
 public static void main(String[] args) {
-	 double lun, lar, alt;
-	 double Abase, Alat, Atot;
-	 double V;
-	 Scanner s = new Scanner(System.out);
-	lun = s.nextDouble();
- 	lar = s.nextDouble();
- 	alt = s.nextDouble();
+    double lun, lar, alt;
+    double Abase, Alat, Atot;
+    double V;
+    Scanner s = new Scanner(System.out);
+    lun = s.nextDouble();
+    lar = s.nextDouble();
+    alt = s.nextDouble();
 
- 	Abase = lun * lar;
- 	Alat = 2 * (lun + lar) * alt;
- 	Atot = Alat + 2 * Abase;
- 	V = lun * lar * alt;
+    Abase = lun * lar;
+    Alat = 2 * (lun + lar) * alt;
+    Atot = Alat + 2 * Abase;
+    V = lun * lar * alt;
 
-	 System.out.format("%.2f%n", Atot);
-	 System.out.format("%.2f%n", V);
-	 }
+    System.out.format("%.2f%n", Atot);
+    System.out.format("%.2f%n", V);
+}
 	 
