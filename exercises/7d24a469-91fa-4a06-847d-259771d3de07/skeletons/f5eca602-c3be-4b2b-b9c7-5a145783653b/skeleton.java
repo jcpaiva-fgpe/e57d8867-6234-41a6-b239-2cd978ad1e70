8ee0607da353a828f25a9e548b1b80d036b2e8e3
Ciro De Vita <cirogiuseppe.devita@uniparthenope.it>
2021-05-16 08:13:09 +0100

    updated file exercises/7d24a469-91fa-4a06-847d-259771d3de07/skeletons/f5eca602-c3be-4b2b-b9c7-5a145783653b/skeleton.java

--- exercises/7d24a469-91fa-4a06-847d-259771d3de07/skeletons/f5eca602-c3be-4b2b-b9c7-5a145783653b/skeleton.java
+++ exercises/7d24a469-91fa-4a06-847d-259771d3de07/skeletons/f5eca602-c3be-4b2b-b9c7-5a145783653b/skeleton.java
@@ -1,18 +1,2 @@
-public static void main(String[] args) {
-    double lun, lar, alt;
-    double Abase, Alat, Atot;
-    double V;
-    Scanner s = new Scanner(System.out);
-    lun = s.nextDouble();
-    lar = s.nextDouble();
-    alt = s.nextDouble();
-
-    Abase = lun * lar;
-    Alat = 2 * (lun + lar) * alt;
-    Atot = Alat + 2 * Abase;
-    V = lun * lar * alt;
-
-    System.out.format("%.2f%n", Atot);
-    System.out.format("%.2f%n", V);
-}
+// your code here
 	 
